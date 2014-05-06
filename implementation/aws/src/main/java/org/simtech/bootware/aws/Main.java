7bfdbc3902b3b38bd6b7e932f8a5a7ab3c206393
lukasreinfurt <git@mail.decouplr.com>
2014-05-06 11:20:17 +0200

    Change test ssh command

--- implementation/aws/src/main/java/org/simtech/bootware/aws/Main.java
+++ implementation/aws/src/main/java/org/simtech/bootware/aws/Main.java
@@ -9,6 +9,8 @@ public class Main {
 		ec2.create();
 
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Press <ENTER> to connect via SSH.");
+		sc.nextLine();
 
 		SSH ssh = new SSH();
 		ssh.connect(ec2.getPublicDNS(), "ec2-user", ec2.getPrivateKey());
@@ -16,9 +18,8 @@ public class Main {
 		System.out.println("Press <ENTER> to execute commands.");
 		sc.nextLine();
 
-		ssh.execute("ls /home/ec2-user/");
 		//ssh.upload("aws-0.0.1.jar", "/home/ec2-user/");
-		ssh.execute("ls /home/ec2-user/");
+		ssh.execute("uname -a && date && uptime && who");
 		ssh.disconnect();
 
 		System.out.println("Press <ENTER> to terminate the EC2 instance.");
