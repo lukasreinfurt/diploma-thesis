b7a0ec344731f4aeb40fb850ac007f6776964d92
lukasreinfurt <git@mail.decouplr.com>
2014-05-12 14:54:58 +0200

    Generate javadocs during compile step

--- implementation/core/src/main/java/org/simtech/bootware/core/Main.java
+++ implementation/core/src/main/java/org/simtech/bootware/core/Main.java
@@ -1,7 +1,15 @@
 package org.simtech.bootware.core;
-
+/**
+ * @author  Lukas Reinfurt
+ * @version 1.0.0
+ */
 public class Main {
 
+	/**
+	 * Runs the bootware program
+	 *
+	 * @param args String[] Commandline arguments
+	 */
 	public static void main(String[] args) {
 		System.out.println("Bootware");
 	}
