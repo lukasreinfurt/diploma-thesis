8fe4fd13eba296c6bbaeba38ada5d528e369f63a
lukasreinfurt <git@mail.decouplr.com>
2014-05-31 15:48:11 +0200

    Fix method param pad warnings

--- implementation/core/src/main/java/org/simtech/bootware/core/Main.java
+++ implementation/core/src/main/java/org/simtech/bootware/core/Main.java
@@ -18,7 +18,7 @@ public final class Main {
 	 *
 	 * @param args Commandline arguments.
 	 */
-	public static void main (final String[] args) {
+	public static void main(final String[] args) {
 
 		// Publish the bootware as a web service.
 		final BootwareImpl implementor = new BootwareImpl();
