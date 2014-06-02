41d70607f44c3d0f436cbc95238066df6c59ad30
lukasreinfurt <git@mail.decouplr.com>
2014-06-02 19:31:24 +0200

    Add bootware remote

---
+++ implementation/remote/src/main/java/org/simtech/bootware/remote/Main.java
@@ -0,0 +1,61 @@
+package org.simtech.bootware.remote;
+
+/**
+ * @author  Lukas Reinfurt
+ * @version 1.0.0
+*/
+
+import javax.xml.ws.Endpoint;
+
+public final class Main {
+
+	private Main() {
+		// not called
+	}
+
+	/**
+	 * Runs the bootware program.
+	 *
+	 * @param args Commandline arguments.
+	 */
+	public static void main(final String[] args) {
+
+		// Publish the bootware as a web service.
+		final RemoteBootwareImpl implementor = new RemoteBootwareImpl();
+		final String address = "http://localhost:8080/axis2/services/Bootware";
+		final Endpoint endpoint = Endpoint.publish(address, implementor);
+		System.out.println("WebService now running at " + address + " ...");
+		implementor.run();
+
+		// Wait for interrupt (e.g. Ctrl+C in the console)
+		final Thread thread = new Thread() {
+			@Override
+			public void run() {
+				synchronized (this) {
+					while (!Thread.currentThread().isInterrupted()) {
+						try {
+							wait();
+						}
+						catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		};
+
+		// On interrupt, stop bootware and web service.
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				implementor.stop();
+				endpoint.stop();
+				System.out.println("WebService stopped.");
+				thread.interrupt();
+			}
+		});
+
+		thread.start();
+	}
+
+}
---
+++ implementation/remote/src/main/java/org/simtech/bootware/remote/RemoteBootware.java
@@ -0,0 +1,13 @@
+package org.simtech.bootware.remote;
+
+import javax.jws.WebService;
+
+import org.simtech.bootware.core.Bootware;
+
+/**
+ * Interface that is implemented by the bootware.
+ */
+@WebService
+public interface RemoteBootware extends Bootware {
+
+}
