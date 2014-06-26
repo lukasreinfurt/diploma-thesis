da2637a16b0242fd7ecb6d36d518cc1d8f8ea10d
lukasreinfurt <git@mail.decouplr.com>
2014-06-26 17:01:29 +0200

    Fix typos

--- diploma thesis/src/customdelatex.rb
+++ diploma thesis/src/customdelatex.rb
@@ -9,7 +9,8 @@ subsubsection = /\\subsubsection\*?\{([\w \.,:;\-]+)\}/
 autoref = /\\autoref\{([\w \.,:;\-]+)\}/
 textit = /\\textit\{([\w \.,:;\-]+)\}/
 autocite = /~\\autocite\{([\w \.,:;\-]+)\}/
-nom = /\\nom\{([\w \.,:;\-]+)\}\{([\w \.,:;\-]+)\}/
+citeauthor = /\\citeauthor\*\{[\w:]+\}/
+nom = /\\nom\{([\w \.,:;\-\\\&]+)\}\{([\w \.,:;\-]+)\}/
 footnote_url = /\\footnote\{\\url\{[\w \.,:;\-\/\?=]+\}\}/
 
 # minted code listings
@@ -24,11 +25,15 @@ table_1 = /\{(\w+(\[[\w]+\])?)+\|\[[\w\.]+\](\w+(\[[\w]*\])?)+\}/
 table_2 = /\s*\&.*/
 table_3 = /\\multirow.*/
 
+# \xyz[]{}
+generic = /~?\\[a-z]+\*?(\[[\w \.,:;\-!=\/]*\]|\{[\w \.,:;\-!=\/]*\})*/
+
 # every line starting with a tab
 tab = /^\t.*/
 
-# \xyz[]{}
-generic = /~?\\[a-z]+\*?(\[[\w \.,:;\-!=\/]*\]|\{[\w \.,:;\-!=\/]*\})*/
+# everything in {}
+brackets = /\{[\wäüöß =\-\\\.,:;\?\(\)\{\}]*\}/
+sth_1 = /\=\d*\w*/
 
 # lines containing only whitespace
 whitespace = /^\s+$/
@@ -53,6 +58,7 @@ File.open('tmp.tex', 'r') do |f|
 				line.gsub!(subsubsection, '\1')
 				line.gsub!(textit, '\1')
 				line.gsub!(autoref, 'Autoref')
+				line.gsub!(citeauthor, '(Author)')
 				line.gsub!(nom, '\1 (\2)')
 				line.gsub!(footnote_url, ' (URL)')
 				line.gsub!(inputminted, '')
@@ -65,6 +71,8 @@ File.open('tmp.tex', 'r') do |f|
 				line.gsub!(table_3, '')
 				line.gsub!(generic, '')
 				line.gsub!(tab, '')
+				line.gsub!(brackets, '')
+				line.gsub!(sth_1, '')
 				line.gsub!(whitespace, '')
 
 				if line == '' && last_line == ''
