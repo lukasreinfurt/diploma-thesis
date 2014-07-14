c0bdb1b32cf78d9716dfe40f1275acfb96d555ab
lukasreinfurt <git@mail.decouplr.com>
2014-07-14 15:17:37 +0200

    Remove get from ConfigurationWrapper

--- implementation/Bootware/core/src/main/java/org/simtech/bootware/core/ConfigurationWrapper.java
+++ implementation/Bootware/core/src/main/java/org/simtech/bootware/core/ConfigurationWrapper.java
@@ -3,8 +3,6 @@ package org.simtech.bootware.core;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.simtech.bootware.core.exceptions.ConfigurationException;
-
 public class ConfigurationWrapper {
 
 	private Map<String, String> configuration = new HashMap<String, String>();
@@ -19,11 +17,4 @@ public class ConfigurationWrapper {
 		return configuration;
 	}
 
-	public final String get(final String entry) throws ConfigurationException {
-		final String value = configuration.get(entry);
-		if (value == null) {
-			throw new ConfigurationException("Entry " + entry + " could not be found in configuration.");
-		}
-		return value;
-	}
 }
