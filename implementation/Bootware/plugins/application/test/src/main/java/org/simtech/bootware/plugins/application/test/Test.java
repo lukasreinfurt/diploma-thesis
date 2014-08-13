817a8d02948f3b9bf2ce6e55e7b4a58df247ee93
lukasreinfurt <git@mail.decouplr.com>
2014-08-13 11:13:22 +0200

    Add application test plugin

---
+++ implementation/Bootware/plugins/application/test/src/main/java/org/simtech/bootware/plugins/application/test/Test.java
@@ -0,0 +1,69 @@
+package org.simtech.bootware.plugins.application.test;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+import org.simtech.bootware.core.ConfigurationWrapper;
+import org.simtech.bootware.core.Connection;
+// import org.simtech.bootware.core.events.ApplicationPluginEvent;
+// import org.simtech.bootware.core.events.Severity;
+import org.simtech.bootware.core.exceptions.ExecuteCommandException;
+import org.simtech.bootware.core.exceptions.ProvisionApplicationException;
+import org.simtech.bootware.core.exceptions.StartApplicationException;
+import org.simtech.bootware.core.plugins.AbstractBasePlugin;
+import org.simtech.bootware.core.plugins.ApplicationPlugin;
+
+public class Test extends AbstractBasePlugin implements ApplicationPlugin {
+
+	public Test() {}
+
+	public final void initialize(final Map<String, ConfigurationWrapper> configurationList) {
+		// no op
+	}
+
+	public final void shutdown() {
+		// no op
+	}
+
+	public final void provision(final Connection connection) throws ProvisionApplicationException {
+
+		if (connection != null) {
+			try {
+				connection.execute("mkdir -p ");
+			}
+			catch (ExecuteCommandException e) {
+				throw new ProvisionApplicationException(e);
+			}
+		}
+		else {
+			throw new ProvisionApplicationException("Connection is null.");
+		}
+	}
+
+	public final void deprovision(final Connection connection) {
+		// no op
+	}
+
+	public final URL start(final Connection connection) throws StartApplicationException {
+
+		if (connection != null) {
+			try {
+				final URL url = new URL("http://example.com");
+				return url;
+			}
+			catch (MalformedURLException e) {
+				throw new StartApplicationException(e);
+			}
+		}
+		else {
+			throw new StartApplicationException("Connection null.");
+		}
+	}
+
+	public final void stop(final Connection connection) {
+		// no op
+	}
+
+}
