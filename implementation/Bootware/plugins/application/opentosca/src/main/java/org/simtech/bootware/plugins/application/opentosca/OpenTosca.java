94565f692d37ab81935ace8bc55b8748ce81fed0
lukasreinfurt <git@mail.decouplr.com>
2014-08-20 17:38:57 +0200

    Add bugfix to opentosca application plugin

--- implementation/Bootware/plugins/application/opentosca/src/main/java/org/simtech/bootware/plugins/application/opentosca/OpenTosca.java
+++ implementation/Bootware/plugins/application/opentosca/src/main/java/org/simtech/bootware/plugins/application/opentosca/OpenTosca.java
@@ -1,8 +1,13 @@
 package org.simtech.bootware.plugins.application.opentosca;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Map;
+import java.util.Properties;
 
 import org.simtech.bootware.core.ConfigurationWrapper;
 import org.simtech.bootware.core.Connection;
@@ -11,6 +16,7 @@ import org.simtech.bootware.core.Connection;
 import org.simtech.bootware.core.exceptions.ExecuteCommandException;
 import org.simtech.bootware.core.exceptions.ProvisionApplicationException;
 import org.simtech.bootware.core.exceptions.StartApplicationException;
+import org.simtech.bootware.core.exceptions.UploadFileException;
 import org.simtech.bootware.core.plugins.AbstractBasePlugin;
 import org.simtech.bootware.core.plugins.ApplicationPlugin;
 
@@ -26,16 +32,78 @@ public class OpenTosca extends AbstractBasePlugin implements ApplicationPlugin {
 		// no op
 	}
 
+	private long getSize(final InputStream is) {
+		final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		final int bufferSize = 4096;
+
+		byte[] buffer = new byte[bufferSize];
+		int n;
+
+		try {
+			while ((n = is.read(buffer)) > 0) {
+				bos.write(buffer, 0, n);
+				buffer = bos.toByteArray();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return buffer.length;
+	}
+
 	public final void provision(final Connection connection) throws ProvisionApplicationException {
 
 		if (connection != null) {
 			try {
-				connection.execute("curl install.opentosca.de/installEC2|sh");
+				// regular installation
+				connection.execute("sudo apt-get update &> /tmp/install.log");
+				connection.execute("curl install.opentosca.de/installEC2|sh &> /tmp/install.log");
+
+				// bug fix start
+
+				// create properties file with the actual ip address
+				final Properties properties = new Properties();
+				properties.setProperty("containerapi.instancedata.epr", "http://" + connection.getURL() + ":1337/containerapi/instancedata");
+				properties.setProperty("containerapi.portability.epr", "http://" + connection.getURL() + ":1337/containerapi/portability");
+
+				// store properties in inputstream
+				final ByteArrayOutputStream output = new ByteArrayOutputStream();
+				properties.store(output, null);
+				final ByteArrayInputStream input = new ByteArrayInputStream(output.toByteArray());
+				final ByteArrayInputStream input2 = new ByteArrayInputStream(output.toByteArray());
+
+				// upload properties
+				connection.upload(input, getSize(input2), "/tmp/opentosca.properties");
+				//connection.execute("sudo mkdir -p /etc/tomcat7");
+				connection.execute("sudo cp /tmp/opentosca.properties /etc/tomcat7/opentosca.properties");
+
+				// upload replacement jar
+				final String jarName = "org.opentosca.siengine.service.impl_1.0.0.201407231442.jar";
+				final InputStream is1 = OpenTosca.class.getResourceAsStream("/bugfix/" + jarName);
+				final InputStream is2 = OpenTosca.class.getResourceAsStream("/bugfix/" + jarName);
+				connection.upload(is1, getSize(is2), "/tmp/" + jarName);
+				//connection.execute("sudo mkdir -p ~/OpenTosca/lib");
+				connection.execute("sudo cp /tmp/" + jarName + " ~/OpenTOSCA/lib/" + jarName);
+
+				// restart opentosca
+				connection.execute("sudo service tomcat7 stop");
+				connection.execute("pkill -f \"java\"");
+				connection.execute("sudo service tomcat7 start");
+				connection.execute("sudo wget -qO- http://install.opentosca.de/start | sh");
+
+				// bug fix end
+			}
+			catch (IOException e) {
+				throw new ProvisionApplicationException(e);
 			}
 			catch (IllegalArgumentException e) {
 				throw new ProvisionApplicationException(e);
 			}
 			catch (ExecuteCommandException e) {
+				throw new ProvisionApplicationException(e);
+			}
+			catch (UploadFileException e) {
 				throw new ProvisionApplicationException(e);
 			}
 		}
@@ -52,7 +120,7 @@ public class OpenTosca extends AbstractBasePlugin implements ApplicationPlugin {
 
 		if (connection != null) {
 			try {
-				final URL url = new URL("http://" + connection.getURL() + ":8080");
+				final URL url = new URL("http://" + connection.getURL());
 				return url;
 			}
 			catch (MalformedURLException e) {
