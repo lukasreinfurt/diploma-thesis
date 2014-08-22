dc6f5c31baa49c1dbf9f92bccf837a7f3ed26d05
lukasreinfurt <git@mail.decouplr.com>
2014-08-22 10:31:04 +0200

    Fix wrong URL

--- implementation/Bootware/plugins/application/test/src/main/java/org/simtech/bootware/plugins/application/test/Test.java
+++ implementation/Bootware/plugins/application/test/src/main/java/org/simtech/bootware/plugins/application/test/Test.java
@@ -50,7 +50,7 @@ public class Test extends AbstractBasePlugin implements ApplicationPlugin {
 
 		if (connection != null) {
 			try {
-				final URL url = new URL("192.168.80.80");
+				final URL url = new URL("http://192.168.80.80");
 				return url;
 			}
 			catch (MalformedURLException e) {
