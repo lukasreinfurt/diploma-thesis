1fd632a174ac5f1a74fccc5bf52d9f94c48d528d
lukasreinfurt <git@mail.decouplr.com>
2014-09-02 10:52:16 +0200

    Add empty provision plugin

---
+++ implementation/Bootware/plugins/provision/empty/src/main/java/org/simtech/bootware/plugins/provision/empty/Empty.java
@@ -0,0 +1,32 @@
+package org.simtech.bootware.plugins.provision.empty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.simtech.bootware.core.ConfigurationWrapper;
+import org.simtech.bootware.core.exceptions.InitializeException;
+import org.simtech.bootware.core.plugins.AbstractBasePlugin;
+import org.simtech.bootware.core.plugins.ProvisionPlugin;
+
+public class Empty extends AbstractBasePlugin implements ProvisionPlugin {
+
+	public Empty() {}
+
+	public final void initialize(final Map<String, ConfigurationWrapper> configurationList) throws InitializeException {
+		// no op
+	}
+
+	public final void shutdown() {
+		// no op
+	}
+
+	public final Map<String, String> provision(final String provisioningEngineEndpoint, final String servicePackageReference) {
+		final Map<String, String> response = new HashMap<String, String>();
+		return response;
+	}
+
+	public final void deprovision(final String provisioningEngineEndpoint, final String servicePackageReference) {
+		// no op
+	}
+
+}
