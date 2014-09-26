115c1930fe77ccb01e72663585d7557f12c0fa4f
lukasreinfurt <git@mail.decouplr.com>
2014-09-26 11:32:15 +0200

    Fix provisioning manager setup

--- implementation/Bootware/plugins/provision/opentosca-simtech/src/main/java/org/simtech/bootware/plugins/provision/opentoscasimtech/SetupProvisioningManager.java
+++ implementation/Bootware/plugins/provision/opentosca-simtech/src/main/java/org/simtech/bootware/plugins/provision/opentoscasimtech/SetupProvisioningManager.java
@@ -19,20 +19,13 @@ public class SetupProvisioningManager {
 	public SetupProvisioningManager() {}
 
 	public final void execute(final EventBus eb, final Map<String, String> sshSettings, final ApplicationInstance instance) {
-		System.out.println("##################################################################################");
 
+		// The path to the service mix installation (i.e. where the bin, data, etc ... folders are)
+		final String serviceMixPath = "/opt/apache-servicemix";
 		final SshConnection connection = new SshConnection(eb);
 
 		try {
 			connection.connect(sshSettings);
-
-			// Set Remote Bootware URL
-			// sed -r -i 's/http.*Repository/Test/g;' /tmp/blueprint.xml
-			// sed -r -i 's/http.*Bootware\?wsdl/Test/g;' /tmp/blueprint.xml
-			// sed -i 's,http://.*/Bootware?wsdl,http://www.replace.com/page,g' /tmp/blueprint.xml
-			final String remoteBootwareIP = instance.getInstanceInformation().get("remoteBootwareIP");
-			final String remoteBootwareUrl = "http://" + remoteBootwareIP + ":8080/axis2/services/Bootware?wsdl";
-			connection.execute("sed -i 's,http://.*/Bootware?wsdl," + remoteBootwareUrl + ",g' /tmp/blueprint.xml");
 
 			// Copy provision context
 			// Marshall to XML
@@ -45,7 +38,30 @@ public class SetupProvisioningManager {
 			final File file = new File("provisioningContext.xml");
 			marshaller.marshal(root, file);
 			// Upload
-			connection.upload(new FileInputStream(file), "/tmp/provisioningContext.xml");
+			final String provisioningContextPath = "/tmp/provisioningContext.xml";
+			connection.upload(new FileInputStream(file), provisioningContextPath);
+			// Set provisioning context location
+			connection.execute("sudo sed -i 's,^provisioningcontext.location.*,provisioningcontext.location=" + provisioningContextPath + ",g' " + serviceMixPath + "/etc/provisioningmanager.properties");
+
+			// Set Remote Bootware URL
+			final String remoteBootwareIP = instance.getInstanceInformation().get("remoteBootwareIP");
+			final String remoteBootwareUrl = "http://" + remoteBootwareIP + ":8080/axis2/services/Bootware?wsdl";
+			connection.execute("sudo sed -i 's,^bwr.endpoint.*,bwr.endpoint=" + remoteBootwareUrl + ",g' " + serviceMixPath + "/etc/provisioningmanager.properties");
+
+			// Set service repository URL
+			final String repositoryEndpoint = "http://localhost:8077/ServiceRepositoryMockup/services/ServiceRepository";
+			connection.execute("sudo sed -i 's,^repository.endpoint.*,repository.endpoint=" + repositoryEndpoint + ",g' " + serviceMixPath + "/etc/provisioningmanager.properties");
+
+			// restart org.simtech.pmbundle
+			// save the resulting command to be passed on later
+			connection.execute("command=$("
+				// ssh into servicemix and return the entry for org.simtech.pmbundle from the list of bundles
+				+ "sshpass -p smx ssh -p 8101 smx@localhost 'list | grep org.simtech.pmbundle'"
+				// from this entry, extract the bundle id and build the command string 'restart [id]'
+				+ "| sudo sed -nr 's/^\\[ *([0-9]+)\\].*/restart \\1/p'"
+				+ ");"
+				// ssh into service mix again and execute the command string
+				+ "sshpass -p smx ssh -p 8101 smx@localhost $command");
 
 			connection.disconnect();
 		}
@@ -53,6 +69,5 @@ public class SetupProvisioningManager {
 			System.out.println(e.getMessage());
 		}
 
-		System.out.println("##################################################################################");
 	}
 }
